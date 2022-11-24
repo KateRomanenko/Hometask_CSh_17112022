53edf9101438a1c06c0fdb9e109acca4456c548a
Ekaterina Romanenko <ev.romanenko@yandex.com>
2022-11-24 19:46:13 +0300

    Hometask 7

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+void Print(double[,] arr)
+{
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+}
+int m = 3;
+int n = 4;
+
+double [,] mass = new double[m, n];
+
+Console.WriteLine(mass.GetLength(0));
+Console.WriteLine(mass.GetLength(1));
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+         mass[i, j] = new Random().Next(-9, 10) + new Random().NextDouble();
+    }
+    Console.WriteLine(); 
+}
+
+Print(mass);
+Console.WriteLine();
+Print(mass);
+
+
+// Задача 50. Напишите программу, которая на вход 
+// принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
